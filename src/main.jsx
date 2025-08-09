db72e182410d7431e49017fd3524c3a076b978ba
Yabets Maregn <yabetsanon@gmail.com>
2025-08-09 12:29:24 +0900

    Update main.jsx if needed for new app structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+// Create a root element and render the App component
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
